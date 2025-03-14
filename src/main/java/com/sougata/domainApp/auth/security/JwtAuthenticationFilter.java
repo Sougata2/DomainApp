0d3fce93f868899f547d76a0bfacfc3672d0578f
sougata <sougata19582001@gmail.com>
2025-03-14 23:20:19 +0530

    comments for security context holder working

--- src/main/java/com/sougata/domainApp/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sougata/domainApp/auth/security/JwtAuthenticationFilter.java
@@ -20,6 +20,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //SO ACTUALLY WHAT IS HAPPENING HERE
+        // 1. get the token from the request
+        // 2. validating the token (extracting the username from the token and loading the matching user from db)
+        // 3. create a authToken[UsernamePasswordAuthenticationToken] for  the security context
+        // 4. pass the authToken to the security context holder
+        // 5. security context holder will check if the user have access to the resource mentioned in request matcher
+        // 6. if the user does not have the proper access then throw error(403 forbidden).
+        // 7. else carry on.
+
         String token = extractToken(request);
         if (token != null) {
             UserDetails userDetails = authService.validateToken(token);
